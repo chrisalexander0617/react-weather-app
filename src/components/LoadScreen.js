ce29023ba2fab80482198fccda22fa1460d935b9
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-09-18 14:36:33 -0400

    Added loading bar

--- src/components/LoadScreen.js
+++ src/components/LoadScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoadingBar from './LoadingBar'
 
 export default function LoadScreen(){
     return (
         <div className="loading-screen">
             <div className="loading-text">Loading</div>
+            <LoadingBar />
         </div>
     )
 }
---
+++ src/components/LoadingBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loadingBar(){
+    return (
+        <div class="loadingOuter">
+            <div class="loadingInner"></div>
+        </div>
+    )
+}
