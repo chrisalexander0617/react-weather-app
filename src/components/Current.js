5687567b0d101a182e0141dcdbad30022e9ec565
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-09-18 13:04:25 -0400

    Added axios, api fetch, comments

--- src/components/Current.js
+++ src/components/Current.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import axios from 'axios'
 
-//Contains our API key from OpenWeatherMap
 //import env from "react-dotenv";
 
 import LoadScreen from './LoadScreen'
@@ -14,15 +14,38 @@ export default class Current extends React.Component {
     }
 
     componentDidMount(){
-        setTimeout(() => {
-            this.setState({isLoading:false})
-        }, 1000)
+        //Refers to this current component. Prevents undefined setState error
+        var currentComponent = this;
+
+        //Checks to ensure geolocator is available before attempting to grab coordinates
+        if ("geolocation" in navigator) {
+
+            navigator.geolocation.getCurrentPosition(function(position) {
+
+                const api = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=bc301f252e62782b84a8350d15fe3e06`
+                
+                axios.get(api).then(res => {
+
+                    //Removes loading screen once data has been successfully retrieved
+                    currentComponent.setState({isLoading:false})
+                    
+                })
+            });
+        }
+            
+        else 
+            console.error("Geolocation is not available");
     }
 
     render(){
         return (
             <div>
-               { this.state.isLoading &&  <LoadScreen /> }
+            { 
+                /* Ternary operator that conditional 
+                renders the loading screen based on current 
+                state value */
+               this.state.isLoading &&  <LoadScreen /> 
+            }
             </div>
         )
     }
