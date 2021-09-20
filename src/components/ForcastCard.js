2ec9e1ec5a49156385c438ac71e321e4c6962212
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-09-20 10:15:41 -0400

    secured url to show icons in safari

--- src/components/ForcastCard.js
+++ src/components/ForcastCard.js
@@ -9,7 +9,7 @@ export default function ForcastCard(props)
         <React.Fragment>
             <Col sm={6} md={4} lg={3}>
                 <Card className="forcast-card my-4 pb-4">
-                    <img className="forcast-card-weather-icon" src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt={'icon'} />
+                    <img className="forcast-card-weather-icon" src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`} alt={'icon'} />
                     <h1 className="focast-card-temp">{props.temp.toFixed()} F</h1>
                     <h4 className="forcast-card-day text-light-blue">{ moment(props.date).format("dddd") }</h4>
                     <h5 className="forcast-card-date text-light-blue" >{ moment(props.date).format("MMMM Do YYYY") }</h5>
